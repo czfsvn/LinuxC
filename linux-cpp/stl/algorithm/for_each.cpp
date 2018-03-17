da8e320730d0119708492f17f2b4924ed70e37a9
czfsvn <rickypc@126.com>
2018-03-17 10:53:22 +0800

    foreach

--- linux-cpp/stl/algorithm/for_each.cpp
+++ linux-cpp/stl/algorithm/for_each.cpp
@@ -27,10 +27,24 @@ void print(T& t)
     std::cout << t << std::endl;
 }
 
+template <typename T, int val>
+void print_1(T& t)
+{
+    std::cout << t << std::endl;
+}
+
+template <typename T>
+void print_2(T& t, const char* str)
+{
+    std::cout << t << std::endl;
+}
+
 void print_(int x, const char* str)
 {
     std::cout << str << ":" << x << std::endl;
 }
+
+
 
 namespace ns_test1
 {
@@ -52,6 +66,19 @@ namespace ns_test1
         std::string str;
     };
 
+    template<typename T, typename V>
+        class Play3
+        {
+            private:
+                V   _str;
+            public:
+                Play3(V str)   :    _str(str)   {}
+                void operator() (T  ele) 
+                {
+                    std::cout << _str << "-" << ele << std::endl;
+                }
+        };
+
     void Test_1()
     {
         int a[] = {1, 2, 3, 4};
@@ -62,6 +89,7 @@ namespace ns_test1
 
         // use function template
         std::for_each(v.begin(), v.end(), print<int>);
+        std::for_each(v.begin(), v.end(), print_1<int, 22>);
 
         // use function , with other params
         std::for_each(v.begin(), v.end(), bind2nd(ptr_fun(print_), "bind2nd"));
@@ -72,11 +100,118 @@ namespace ns_test1
         // use function object
         std::for_each(v.begin(), v.end(), Play1());
         std::for_each(v.begin(), v.end(), Play2("play2"));
+        std::for_each(v.begin(), v.end(), Play3<int, const char*>("element"));
     }
 };
+
+namespace ns_test2
+{
+    class Door
+    {
+        public:
+            void open() const
+            {
+                cout << "open door horizontally\n";
+            }
+    };
+
+    class DoorContoller
+    {
+        protected:
+            std::vector<Door> doorVec_;
+
+        public:
+            void    addDoor(Door door)
+            {
+                doorVec_.push_back(door);
+            }
+
+            // use mem_fun          if pointer in container 
+            // use mem_fun_ref      if object  in container
+            void    openDoor()  const
+            {
+                std::for_each(doorVec_.begin(), doorVec_.end(), mem_fun_ref(&Door::open));
+            }
+    };
+
+    void Test_1()
+    {
+        DoorContoller dc;
+        dc.addDoor(Door());
+        dc.addDoor(Door());
+        dc.openDoor();
+    }
+}
+
+namespace ns_test3
+{
+    class AbstractDoor
+    {
+        public:
+            virtual void open(const char* str) const    =   0;
+            virtual ~AbstractDoor() {}
+    };
+
+    class HorizontalDoor    : public AbstractDoor
+    {
+        public:
+            virtual void open(const char* str) const
+            {
+                std::cout << "open door horizontally\n";
+            }
+    };
+
+    class VerticalDoor : public AbstractDoor
+    {
+        public:
+            virtual void open(const char* str) const
+            {
+                std::cout << "open door vertically\n";
+            }
+    };
+
+    class DoorContoller
+    {
+        protected:
+            vector<AbstractDoor*>   doorVec_;
+        public:
+            void    addDoor(AbstractDoor* door)
+            {
+                doorVec_.push_back(door);
+            }
+
+            // use mem_fun          if pointer in container 
+            // use mem_fun_ref      if object  in container
+            void openDoor() const
+            {
+                std::for_each(doorVec_.begin(), doorVec_.end(), std::bind2nd(mem_fun(&AbstractDoor::open), "John"));
+            }
+
+            ~DoorContoller()
+            {
+                std::for_each(doorVec_.begin(), doorVec_.end(), 
+                        [](AbstractDoor* door)
+                        {
+                            delete door;
+                            door = nullptr;
+                        }
+                        );
+                doorVec_.clear();
+            }
+    };
+
+    void Test_1()
+    {
+        DoorContoller   dc;
+        dc.addDoor(new HorizontalDoor);
+        dc.addDoor(new VerticalDoor);
+        dc.openDoor();
+    }
+}
 
 int main()
 {
     ns_test1::Test_1();
+    ns_test2::Test_1();
     return 1;
 }
